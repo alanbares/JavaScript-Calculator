640473a9b82c665ec3d1a1231e4c262233dd4f43
alanbares <neloice@gmail.com>
2016-02-06 15:46:49 -0800

    Clear display when AC or CE button is clicked

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 /*global $ */
 
-
-
 $(document).ready(function() {
 	var calcValues = [],
-		displayBox = $('.display-text')
+		displayBox = $('.display-text'),
+		numKeys = [0,1,2,3,4,5,6,7,8,9],
+		operationKeys = ['+', '-', '/', '*', '%'],
+		controlKeys = ['AC', 'CE'];
 
 	$('button').click(function(e) {
-		calcValues.push($(this).data('calcVal'));
+		var k = $(this).data('calcVal');
 		console.log(calcValues);
-		displayBox.text(calcValues);
+
+		// On click switch case for buttons not displayed
+		if ($.inArray(k, controlKeys) >=0) {
+			calcValues = [];
+		} else if ($.inArray(k, numKeys)) {
+			calcValues.push(k);
+		}
+		updateDisplay();
 	});
+
+	function updateDisplay() {
+		displayBox.text(calcValues.join(''));
+	}
+
 });
