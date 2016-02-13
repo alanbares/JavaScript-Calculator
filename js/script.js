20c13a7a4c55f86cda195248cf4ab9c02e287c81
alanbares <neloice@gmail.com>
2016-02-12 20:59:39 -0800

    Remove data tag in html and add descriptions to JS

--- js/script.js
+++ js/script.js
@@ -8,15 +8,19 @@ $(document).ready(function() {
 		var k = $(this).data('calcVal'),
 			result;
 
+		//Tests if input is a number
 		if (/[0-9]/.test(k)) {
 			input.push(k);
 			console.log(input);
+
+		//Check if input is = and then compute result
 		} else if(k == '=') {
 			expression.push(input);
 
 			result = eval(expression.join(''));
 			console.log(result);
 		}
+		// Joins input array into a strin and pushes it to expression array
 		 else  {
 			expression.push(input.join(''));
 			input = [];
